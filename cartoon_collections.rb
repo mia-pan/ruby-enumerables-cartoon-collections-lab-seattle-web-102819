adf9035e7ff6ae3dc6f353a00e3e14205716767b
Mia Pan <miapanyuewei@gmail.com>
2019-10-25 23:52:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ def long_planeteer_calls(planeteer_calls)
 
 def find_the_cheese(cheese_item)
 cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_item.find do |cheese| cheese_types.inclu?(cheese)
+  cheese_item.find do |cheese| cheese_types.include?(cheese)
 end
 find_the_cheese(cheese_item)
