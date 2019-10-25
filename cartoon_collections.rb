4444202e43e5825903e9a65e17276cbc880d3166
Mia Pan <miapanyuewei@gmail.com>
2019-10-25 23:40:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,21 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each_with_index do |dwarf, index|
+     puts "#{index +1 }. #{dwarf}"
+  end
   # Your code here
 end
+planeteer_calls = %w[earth wind fire water heart]
+def summon_captain_planet(planeteer_calls)
+planeteer_calls.map { |call| call.capitalize + '!' }
+  end
+summon_captain_planet(planeteer_calls)
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length >4 }
+  end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
