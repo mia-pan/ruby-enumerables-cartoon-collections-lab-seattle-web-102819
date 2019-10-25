e8c3ce24f048428ed7f2458326c2f79c90157798
Mia Pan <miapanyuewei@gmail.com>
2019-10-25 23:43:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(planeteer_calls)
 
 
 
-def find_the_cheese# code an argument here
+def find_the_cheese(# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
